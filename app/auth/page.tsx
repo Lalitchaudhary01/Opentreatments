c59d03c3dc6d8fa868b0b5c22f5dd3e0ccd5e6f5
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-09-04 23:01:44 +0530

    fix error

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 
 import AuthForm from "@/features/auth/components/LoginForm";
+import { Suspense } from "react";
 
 export const dynamic = "force-dynamic"; // ⚡ disables pre-rendering
 
 export default function LoginPage() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <AuthForm />
+      <Suspense>
+        <AuthForm />
+      </Suspense>
     </div>
   );
 }
