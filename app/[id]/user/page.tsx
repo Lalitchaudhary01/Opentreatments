03af9cb52b43f8172a7af0f3fbe1f5af4dec79f3
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2025-10-29 16:47:15 +0530

    change ui of login fomr and gallery

--- app/[id]/user/page.tsx
+++ app/[id]/user/page.tsx
@@ -1,10 +1,13 @@
 import MyProfile from "@/features/user/components/MyProfile";
 
 interface UserProfilePageProps {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 }
 
-export default function UserProfilePage({ params }: UserProfilePageProps) {
-  // params.id = logged-in user id
+export default async function UserProfilePage({
+  params,
+}: UserProfilePageProps) {
+  const { id } = await params;
+  // you can pass `id` if needed to MyProfile
   return <MyProfile />;
 }
