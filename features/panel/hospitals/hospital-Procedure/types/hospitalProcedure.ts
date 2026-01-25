12abd766f7388ea8c62198e88265698c5ae79e12
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2026-01-25 11:34:54 +0530

    fix

--- features/panel/hospitals/hospital-Procedure/types/hospitalProcedure.ts
+++
@@ -1,9 +0,0 @@
-// /features/hospital-procedures/types/hospitalProcedure.ts
-export interface HospitalProcedure {
-  id: string;
-  name: string;
-  description?: string | null;
-  cost?: number | null;
-  duration?: string | null;
-  hospitalId: string;
-}
