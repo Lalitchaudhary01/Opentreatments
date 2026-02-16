15521a43907b5a9e3684c360ecf8c5116e45c5d8
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2026-02-16 17:46:52 +0530

    update

--- features/panel/lab/lib/validations/index.ts
+++
@@ -1,7 +0,0 @@
-// features/panel/lab/lib/validations/index.ts
-
-export * from './labProfileSchema'
-export * from './testSchema'
-export * from './packageSchema'
-export * from './bookingSchema'
-export * from './technicianSchema'
