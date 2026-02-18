e31cf9bc20fd099c05b714a21f989e0d7cd30b96
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2026-02-18 08:48:29 +0530

    debug

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -4,6 +4,18 @@ declare module "next-auth" {
   interface Session {
     user: {
       id: string; // logged-in user id
+      role?: string | null; // add role property
     } & DefaultSession["user"];
+  }
+
+  // Also extend the User type if you want
+  interface User {
+    role?: string | null;
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    role?: string | null;
   }
 }
