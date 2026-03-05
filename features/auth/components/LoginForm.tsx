a0524dcd44df709c47e9e65d9fb6bd57be24857d
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2026-03-05 13:30:54 +0530

    update serveraction in services

--- features/auth/components/LoginForm.tsx
+++ features/auth/components/LoginForm.tsx
@@ -9,6 +9,7 @@ import {
   DoctorOnboardingSteps,
 } from "../doctor/DoctorOnboardingSteps";
 import { completeDoctorOnboarding } from "../doctor/actions/doctorOnboardingActions";
+import Image from "next/image";
 
 type Role = "USER" | "DOCTOR" | "PHARMACY" | "ADMIN";
 
@@ -349,10 +350,19 @@ export default function AuthForm() {
           <div className="ob-left-glow2" />
 
           <div className="ob-brand">
-            <div className="ob-brand-icon">
-              <svg viewBox="0 0 24 24"><path d="M19 3H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7 3c1.93 0 3.5 1.57 3.5 3.5S13.93 13 12 13s-3.5-1.57-3.5-3.5S10.07 6 12 6zm7 13H5v-.23c0-.62.28-1.2.76-1.58C7.47 15.82 9.64 15 12 15s4.53.82 6.24 2.19c.48.38.76.97.76 1.58V19z"/></svg>
-            </div>
-            <div className="ob-brand-name">Open<span>Treatment</span></div>
+            
+            <div className="flex items-center gap-3">
+                          <Image
+                            src="/Subtract.svg"
+                            alt="Open Treatment"
+                            width={51}
+                            height={72}
+                            className="w-10 h-auto sm:w-12 lg:w-[51px]"
+                          />
+                          <span className="text-xl sm:text-2xl font-bold text-[#ECF5FF]">
+                            Open Treatment
+                          </span>
+                        </div>
           </div>
 
           <svg viewBox="0 0 340 300" style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%,-55%)", width: 340, opacity: .18 }} fill="none">
