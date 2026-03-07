e14ee6811ec9032f6cd2d606298faab6e1f96efd
Lalitchaudhary01 <lalitkumar46789@gmail.com>
2026-03-07 22:45:46 +0530

    Added sitemap

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://opentreatment.in/sitemap.xml",
+  };
+}
